1ed26446bc28e20ab43effc2ee015fe1a15a5a64
João Netto <joaonettopb@hotmail.com>
2018-11-04 13:56:50 -0200

    first commit

--- app.js
+++ app.js
@@ -1,93 +1,46 @@
-/**
- * Copyright 2015 IBM Corp. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+const watson = require('watson-developer-cloud/assistant/v1');
+const prompt = require('prompt-sync')();
+require('dotenv').config();
 
-'use strict';
-
-var express = require('express'); // app server
-var bodyParser = require('body-parser'); // parser for post requests
-var AssistantV1 = require('watson-developer-cloud/assistant/v1'); // watson sdk
-
-var app = express();
-
-// Bootstrap application settings
-app.use(express.static('./public')); // load UI from public folder
-app.use(bodyParser.json());
-
-// Create the service wrapper
-
-var assistant = new AssistantV1({
-  version: '2018-07-10'
+const chatbot = new watson({
+    url: process.env.URL,
+    iam_apikey: process.env.API_KEY,
+    version: process.env.VERSION
 });
 
-// Endpoint to be call from the client side
-app.post('/api/message', function (req, res) {
-  var workspace = process.env.WORKSPACE_ID || '<workspace-id>';
-  if (!workspace || workspace === '<workspace-id>') {
-    return res.json({
-      'output': {
-        'text': 'The app has not been configured with a <b>WORKSPACE_ID</b> environment variable. Please refer to the ' + '<a href="https://github.com/watson-developer-cloud/assistant-simple">README</a> documentation on how to set this variable. <br>' + 'Once a workspace has been defined the intents may be imported from ' + '<a href="https://github.com/watson-developer-cloud/assistant-simple/blob/master/training/car_workspace.json">here</a> in order to get a working application.'
-      }
-    });
-  }
-  var payload = {
-    workspace_id: workspace,
-    context: req.body.context || {},
-    input: req.body.input || {}
-  };
+const workspace_id = process.env.WORKSPACE_ID;
 
-  // Send the input to the assistant service
-  assistant.message(payload, function (err, data) {
+chatbot.message({ workspace_id }, trataResposta);
+
+let fimDeConversa = false;
+
+function trataResposta(err, resposta) {
     if (err) {
-      return res.status(err.code || 500).json(err);
+        console.log(err);
+        return;
     }
 
-    return res.json(updateMessage(payload, data));
-  });
-});
+    if (resposta.intents.length > 0) {
+        console.log('Eu detectei a intenção: ' + resposta.intents[0].intent);
+        if (resposta.intents[0].intent == 'despedida' || resposta.intents[0].intent == 'General_Ending') {
+            fimDeConversa = true;
+        }
 
-/**
- * Updates the response text using the intent confidence
- * @param  {Object} input The request to the Assistant service
- * @param  {Object} response The response from the Assistant service
- * @return {Object}          The response with the updated message
- */
-function updateMessage(input, response) {
-  var responseText = null;
-  if (!response.output) {
-    response.output = {};
-  } else {
-    return response;
-  }
-  if (response.intents && response.intents[0]) {
-    var intent = response.intents[0];
-    // Depending on the confidence of the response the app can return different messages.
-    // The confidence will vary depending on how well the system is trained. The service will always try to assign
-    // a class/intent to the input. If the confidence is low, then it suggests the service is unsure of the
-    // user's intent . In these cases it is usually best to return a disambiguation message
-    // ('I did not understand your intent, please rephrase your question', etc..)
-    if (intent.confidence >= 0.75) {
-      responseText = 'I understood your intent was ' + intent.intent;
-    } else if (intent.confidence >= 0.5) {
-      responseText = 'I think your intent was ' + intent.intent;
-    } else {
-      responseText = 'I did not understand your intent';
+
+        if (resposta.intents[0].intent == 'horario') {
+            console.log(`Agora são ${new Date().toLocaleTimeString()}.`);
+        }
     }
-  }
-  response.output.text = responseText;
-  return response;
+
+    if (resposta.output.text.length > 0) {
+        console.log(resposta.output.text[0]);
+    }
+
+    if (!fimDeConversa) {
+        const mensagemUsuario = prompt('>');
+        chatbot.message({
+            workspace_id,
+            input: { text: mensagemUsuario }
+        }, trataResposta);
+    }
 }
-
-module.exports = app;
